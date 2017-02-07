acfbc960a6bbb305728ebe719e0bb433e809c989
FrankSunDragon <363126550@qq.com>
2017-02-07 16:54:02 +0800

    dd

--- TrySwift3.0/Pods/Target Support Files/Pods-TrySwift3.0/Pods-TrySwift3.0-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_TrySwift3_0VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TrySwift3_0VersionString[];
-
