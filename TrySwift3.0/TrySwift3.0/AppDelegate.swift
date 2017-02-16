4b02fbd08dca69441900dbc048dc95e09c75daf0
FrankSunDragon <363126550@qq.com>
2017-02-16 22:04:03 +0800

    测试

--- TrySwift3.0/TrySwift3.0/AppDelegate.swift
+++ TrySwift3.0/TrySwift3.0/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
      
-     print("ok")
+     print("okdddd")
             window = UIWindow(frame: UIScreen.main.bounds)
             window?.makeKeyAndVisible()
             window?.rootViewController = TSMainTabBarViewController()
